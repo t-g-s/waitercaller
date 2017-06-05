247a48af1e0ba15ca6cce9fff7f4b767bb40b04f
Tom Stoll <tom@ubnt.com>
2017-06-05 16:38:46 -0500

    waitercaller, display errors.

--- waitercaller.py
+++ waitercaller.py
@@ -95,17 +95,16 @@ def load_user(user_id):
 
 @app.route("/register", methods=["POST"])
 def register():
-    email = request.form.get("email")
-    pw1 = request.form.get("password")
-    pw2 = request.form.get("password2")
-    if not pw1 == pw2:
+    form = RegistrationForm(request.form)
+    if form.validate():
+        if DB.get_user(form.email.data):
+            form.email.errors.append("Email address already registered")
+            return render_template('home.html', registrationform=form)
+        salt = PH.get_salt()
+        hashed = PH.get_hash(form.password2.data + salt)
+        DB.add_user(form.email.data, salt, hashed)
         return redirect(url_for('home'))
-    if DB.get_user(email):
-        return redirect(url_for('home'))
-    salt = PH.get_salt()
-    hashed = PH.get_hash(pw1 + salt)
-    DB.add_user(email, salt, hashed)
-    return redirect(url_for('home'))
+    return render_template("home.html", registrationform=form)
 
 @app.route("/newrequest/<tid>")
 def new_request(tid):
